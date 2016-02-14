a2283904939537f3ede212690027a1921a938957
Ruben <rubenmueller90@gmail.com>
2016-02-14 14:08:40 +0100

    s

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+	public override void OnInspectorGUI ()
+	{
+		MapGenerator mapGen = (MapGenerator) target;
+		if(DrawDefaultInspector())
+		{
+		if(mapGen.autoUPdate)
+			{
+				mapGen.GenerateMap();
+			}
+		}
+
+		if(GUILayout.Button("Generate"))
+		{
+			mapGen.GenerateMap();
+		}
+	}
+}
---
+++ Assets/Scripts/MapDisplaz.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplaz : MonoBehaviour {
+	public Renderer textureRenderer;
+
+	public void DrawTexture(Texture2D texture)
+	{
+
+		textureRenderer.sharedMaterial.mainTexture = texture;
+		textureRenderer.transform.localScale = new Vector3(texture.width, 1,texture.height);
+
+	}
+}
